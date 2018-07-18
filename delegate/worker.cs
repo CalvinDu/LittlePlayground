2b3a71274c7a3e401a72a891c4178b8664fa5183
Du Calvin <Calvin.Du@tetrapak.com>
2018-07-18 17:06:13 +0800

    added new stuff

---
+++ delegate/worker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate
+{
+    public delegate void WorkPerformedHandler( int hours ); 
+    public class worker
+    {
+        public event WorkPerformedHandler Workperformed;
+        public virtual void DoWork(int hours)
+        {
+            OnWorkPerformed(hours);
+        }
+
+        protected virtual void OnWorkPerformed( int hours )
+        {
+            WorkPerformedHandler del = Workperformed as WorkPerformedHandler;
+            if (del != null)
+            {
+                del(hours);
+            }
+        }
+        static void WorkPerformed1(int hours)
+        {
+            Console.WriteLine($" WorkPerformed1 did {hours} hours!");
+        }
+
+        static void WorkPerformed2(int hours)
+        {
+            Console.WriteLine($" WorkPerformed2 did {hours} hours!");
+        }
+    }
+
+
+}
