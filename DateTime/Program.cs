d9c7a36f4cb7278331fc4a8e113e4cb9610e3652
Du Calvin <Calvin.Du@tetrapak.com>
2018-07-20 16:43:13 +0800

    added new stuff

---
+++ DateTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = System.DateTime.Now;
+            
+            Console.WriteLine(dateTime.AddDays(-(int)(dateTime.DayOfWeek)+1));
+
+            Console.WriteLine(new System.DateTime(dateTime.Year, dateTime.Month, 1).AddMonths(7));
+            Console.ReadKey();
+        }
+    }
+}
