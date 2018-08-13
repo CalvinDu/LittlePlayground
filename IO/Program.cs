2c00a748f060c4e90db5e8be308c0880b68c1135
Du Calvin <Calvin.Du@tetrapak.com>
2018-08-13 10:28:44 +0800

    updated

---
+++ IO/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fi = new FileInfo(@".\test.txt");
+            Console.WriteLine("Hello World!");
+        }
+
+
+            const int megabyte = 1024 * 1024;
+
+        public void ReadAndProcessLargeFile(string theFilename, long whereToStartReading = 0)
+        {
+            FileStream fileStram = new FileStream(theFilename, FileMode.Open, FileAccess.Read);
+            using (fileStram)
+            {
+                byte[] buffer = new byte[megabyte];
+                fileStram.Seek(whereToStartReading, SeekOrigin.Begin);
+                int bytesRead = fileStram.Read(buffer, 0, megabyte);
+                while (bytesRead > 0)
+                {
+                    ProcessChunk(buffer, bytesRead);
+                    bytesRead = fileStram.Read(buffer, 0, megabyte);
+                }
+
+            }
+
+        }
+            private void ProcessChunk(byte[] buffer, int bytesRead)
+            {
+                // Do the processing here
+            }
+        
+    }
+}
---
+++ IO/Writer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IO
+{
+    public static class Writer
+    {
+        public static void WriteFile()
+        {
+            var 
+        }
+    }
+}
