9c86dcc813b639901a48a19938e5ab322d2afa2f
Cong Du <crazycnok@gmail.com>
2018-09-26 18:58:52 +0800

    update

---
+++ LeetCode/HeapSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public static class HeapSort
+    {
+        private static int heapSize;
+
+        private static void buildHeap(int[] a)
+        {
+            heapSize = a.Length - 1;
+            for (int i = heapSize / 2; i >= 0; i--)
+                heapify(a, i);
+        }
+
+        private static void heapify(int[] a, int index)
+        {
+            int left = 2 * index;
+            int right = 2 * index + 1;
+            int largest = index;
+
+            if (left <= heapSize && a[left] > a[largest])
+                largest = left;
+            if (right <= heapSize && a[right] > a[largest])
+                largest = right;
+            if (largest != index)
+            {
+                swap(a, index, largest);
+                heapify(a, largest);
+            }
+        }
+
+        public static void heapSort(int[] a)
+        {
+            buildHeap(a);
+            for (int i = a.Length - 1; i > 0; i--)
+            {
+                swap(a, 0, i);
+                heapSize--;
+                heapify(a, 0);
+            }
+        }
+        private static void swap(int[] A, int left, int right)
+        {
+            int temp = A[left];
+            A[left] = A[right];
+            A[right] = temp;
+        }
+
+    }
+}
---
+++ LeetCode/quickSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public static class QuickSort
+    {
+        public static void quickSort(int[] A, int left, int right)
+        {
+            if (left > right || left < 0 || right < 0) return;
+
+            int index = partition(A, left, right);
+
+            if (index != -1)
+            {
+                quickSort(A, left, index - 1);
+                quickSort(A, index + 1, right);
+            }
+        }
+        private static int partition(int[] A, int left, int right)
+        {
+            if (left > right) return -1;
+
+            int end = left;
+
+            int pivot = A[right];
+
+            for (int i = left; i < right; i++)
+            {
+                if (A[i] < pivot)
+                {
+                    swap(A, i, end);
+                    end++;
+                }
+            }
+            swap(A, end, right);
+
+            return end;
+        }
+        private static void swap(int[] A, int left, int right)
+        {
+            int temp = A[left];
+            A[left] = A[right];
+            A[right] = temp;
+        }
+    }
+}
